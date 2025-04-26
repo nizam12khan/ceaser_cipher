468de9c241ec47dc3611226e74ae9046e2e2a646
Nizamuddin Khan <108728893+nizam12khan@users.noreply.github.com>
2025-04-26 19:59:28 +0000

    added return

--- ceaser_cipher.rb
+++ ceaser_cipher.rb
@@ -3,8 +3,9 @@ def ceaser_cipher(string, shift)
     ciphered = string.chars
     ciphered.each_with_index do |x,y|
      if alphabet.include?(x.downcase)
-         ciphered[y] = alphabet[(alphabet.find_index(x.downcase) + shift) % 26]
-           ciphered[y] = ciphered[y].upcase if x == x.upcase
+        ciphered[y] = alphabet[(alphabet.find_index(x.downcase) + shift) % 26]
+        ciphered[y] = ciphered[y].upcase if x == x.upcase
      end
-    end      
+    end     
+    ciphered.join 
   end
